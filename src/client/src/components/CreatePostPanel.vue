4eb96686e3afcb24829a2d7cb6b9c69fe5e6e1a1
Hien.Huynh <Hien.Huynh@sandata.com>
2019-10-30 13:38:53 +0700

    Add router and create post panel

---
+++ src/client/src/components/CreatePostPanel.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-card class="mx-auto" outlined>
+        <v-card-title>
+            <span class="headline mb-4">Create Post</span>
+        </v-card-title>
+    </v-card>
+</template>
