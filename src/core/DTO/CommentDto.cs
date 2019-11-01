012a6f4fc933c3f3f4fa205e666fd70da683cb52
Hoach Nguyen <hoach.nguyen@sandata.conm>
2019-11-01 20:30:54 +0700

    add missing file

---
+++ src/core/DTO/CommentDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CaveCore.DTO
+{
+    public class CommentDto
+    {
+        public string Id { get; set; }
+
+        [Required]
+        public string PostId { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public string ParentId { get; set; }
+        public string Username { get; set; }
+        public string Created { get; set; }
+    }
+}
